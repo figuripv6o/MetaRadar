82ad12bd6110f2a61cd55cd8019b04d785c417b8
semper-viventem <kostyaxxx8@gmail.com>
2025-03-19 19:20:35 -0700

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         minSdk = 29
         targetSdk = 35
 
-        versionCode = 1708536365
-        versionName = "0.27.3-beta"
+        versionCode = 1708536366
+        versionName = "0.27.4-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
