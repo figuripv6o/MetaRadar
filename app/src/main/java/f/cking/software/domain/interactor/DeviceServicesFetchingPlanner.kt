b0d29c8ec20e9405769480e0e9e8cd78429bdf44
semper-viventem <kostyaxxx8@gmail.com>
2025-03-31 20:41:33 -0700

    Update app version

--- app/src/main/java/f/cking/software/domain/interactor/DeviceServicesFetchingPlanner.kt
+++ app/src/main/java/f/cking/software/domain/interactor/DeviceServicesFetchingPlanner.kt
@@ -35,7 +35,7 @@ class DeviceServicesFetchingPlanner(
     suspend fun scheduleFetchServiceInfo(devices: List<SavedDeviceHandle>): List<SavedDeviceHandle> = coroutineScope {
 
         val cooldown = this@DeviceServicesFetchingPlanner.cooldown
-        if (cooldown != null && System.currentTimeMillis() - cooldown < MIN_COOLDOWN_DURATION_MINS.seconds.inWholeMilliseconds) {
+        if (cooldown != null && System.currentTimeMillis() - cooldown < MIN_COOLDOWN_DURATION_MINS.minutes.inWholeMilliseconds) {
             Timber.tag(TAG).i("Device services fetching is on cooldown due to a high errors rate, current batch will be skipped")
             return@coroutineScope devices
         }
